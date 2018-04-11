de5abfcf66df478c8e3f0244d10334bda0e72a70
Naokazu Terada <naokazu.terada@gmail.com>
2018-04-11 23:25:31 +0900

    Show rebuild rss as a list

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,34 @@
 <template>
-  <h1>Hello world!</h1>
+  <div>
+    <ul>
+      <li v-for="item in data">
+        <a v-bind:href="item.link">
+          {{item.title}}
+        </a>
+      </li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  asyncData ({params}, callback) {
+    axios.get(`http://feeds.rebuild.fm/rebuildfm`)
+      .then((res) => {
+        var parseString = require('xml2js').parseString
+        var xml = res.data
+        parseString(xml, (message, xmlres) => {
+          callback(null, {data: xmlres.rss.channel[0].item})
+        })
+      })
+      .catch((e) => {
+        callback({ statusCode: 404, message: 'ページが見つかりません' })
+      })
+  }
+}
+</script>
+
+<style>
+</style>
