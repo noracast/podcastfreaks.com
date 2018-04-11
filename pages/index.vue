b6b65dcfa4ea2827f88fe9427119d26e56b311f0
Naokazu Terada <naokazu.terada@gmail.com>
2018-04-12 00:09:23 +0900

    Show cal-heatmap

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,36 @@
 <template>
-  <div>
-    <ul>
-      <li v-for="item in data">
-        <a v-bind:href="item.link">
-          {{item.title}}
-        </a>
-      </li>
-    </ul>
-  </div>
+<div>
+  <div id="cal-heatmap"></div>
+  <ul>
+    <li v-for="item in data">
+      <a v-bind:href="item.link">
+        {{item.title}}
+      </a>
+    </li>
+  </ul>
+</div>
 </template>
 
+<style>
+@import '//cdn.jsdelivr.net/cal-heatmap/3.3.10/cal-heatmap.css';
+</style>
+
 <script>
-import axios from 'axios';
+import axios from 'axios'
+import xml2js from 'xml2js'
+import d3 from 'd3'
+import CalHeatMap from 'cal-heatmap'
 
 export default {
+  mounted () {
+    var cal = new CalHeatMap()
+    cal.init({})
+  },
   asyncData ({params}, callback) {
     axios.get(`http://feeds.rebuild.fm/rebuildfm`)
       .then((res) => {
-        var parseString = require('xml2js').parseString
         var xml = res.data
-        parseString(xml, (message, xmlres) => {
+        xml2js.parseString(xml, (message, xmlres) => {
           callback(null, {data: xmlres.rss.channel[0].item})
         })
       })
@@ -29,6 +40,3 @@ export default {
   }
 }
 </script>
-
-<style>
-</style>
