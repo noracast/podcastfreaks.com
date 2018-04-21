97bf92ec83fa24d8987b9cd022efb3a2fa108057
Naokazu Terada <naokazu.terada@gmail.com>
2018-04-21 12:42:15 +0900

    GitHub pages compatiblity

---
+++ nuxt.config.js
@@ -0,0 +1,8 @@
+// only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/podcast-freaks/'
+  }
+} : {}
+
+module.exports = routerBase
