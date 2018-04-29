9671c95e612f3bbaf9a72920935381f4d8084331
Naokazu Terada <naokazu.terada@gmail.com>
2018-04-29 23:54:52 +0900

    Fixes

--- _assets/push.sh
+++ _assets/push.sh
@@ -7,12 +7,12 @@ setup_git() {
 
 commit_website_files() {
   git checkout -b gh-pages
-  git add . *.html
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+  git add .
+  git commit --allow-empty --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
+  git remote add origin-pages https://${GH_TOKEN}@github.com:developersjp/podcast-freaks.git > /dev/null 2>&1
   git push --quiet --set-upstream origin-pages gh-pages
 }
 
