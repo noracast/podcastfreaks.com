0f34e4e62d33cdbb664ded71575516fb3337124e
Naokazu Terada <naokazu.terada@gmail.com>
2018-09-11 23:31:13 +0900

    Download cover images (WIP: wget error)

--- scripts/pregenerate.js
+++ scripts/pregenerate.js
@@ -4,14 +4,18 @@ const wget = require('wget-improved')
 const fs = require('fs')
 const xml2js = require('xml2js')
 const moment = require('moment')
+const _ = require('lodash')
+const fileExtension = require('file-extension')
 
 const DOWNLOADS_DIR = './static/downloads'
 const RSS_DIR       = './static/downloads/rss'
+const COVER_DIR     = './static/downloads/cover'
 const BUILD_INFO    = './static/downloads/build_info.json'
 
 // Make sure parent dir existence and its clean
 shell.rm('-rf', DOWNLOADS_DIR)
 shell.mkdir('-p', RSS_DIR)
+shell.mkdir('-p', COVER_DIR)
 
 var total = Object.keys(rss).length
 var latest_pubdates = []
@@ -20,11 +24,11 @@ var channels = {}
 
 Object.keys(rss).forEach(function (key) {
   const src = rss[key].feed
-  const dist = `${RSS_DIR}/${key}.rss`
-  const download = wget.download(src, dist)
+  const dist_rss = `${RSS_DIR}/${key}.rss`
+  const download = wget.download(src, dist_rss)
   download.on('end', ()=> {
     // nodeから実行する場合に、importなどが使えなかったために、async/awaitなどを使わないやり方で書いている
-    fs.readFile(`${__dirname}/.${dist}`, (err, xml)=> {
+    fs.readFile(`${__dirname}/.${dist_rss}`, (err, xml)=> {
       if(err) {
         throw err
       }
@@ -32,6 +36,13 @@ Object.keys(rss).forEach(function (key) {
         if(_err) {
           throw _err
         }
+
+        // TODO: Download cover images
+        console.log('-------'+json.rss.channel.title)
+        const cover = _.get(json, 'rss.channel[itunes:image].$.href') || _.get(json, 'rss.channel[itunes:image].href') || _.get(json, 'rss.channel.image.url')
+        console.log(cover)
+        wget.download(cover, `${COVER_DIR}/${key}.${fileExtension(cover)}`)
+
         // Get the latest episode's publish date
         latest_pubdates.push({
           id: key,
