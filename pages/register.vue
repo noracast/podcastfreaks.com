454dfb78771060edd298d3d388e721da9aa1d0ee
Naokazu Terada <naokazu.terada@gmail.com>
2018-12-07 17:36:03 +0900

    Add register page

---
+++ pages/register.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+div
+  h2 Contact
+  p
+    | 自薦他薦問わず、このサイトに登録してほしい番組がある場合は下記のフォームよりお申し込みください。<br>
+    | ただし、運営者側の意図に沿わない場合は登録しかねる場合がありますことはご了承ください。
+</template>
+
+<style lang="sass?indentedSyntax" scoped>
+
+</style>
