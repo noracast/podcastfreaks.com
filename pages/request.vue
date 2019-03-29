b00588bfaa186d711115312d044aba3923dba77e
Naokazu Terada <naokazu.terada@gmail.com>
2019-03-29 21:25:09 +0900

    Tweak comment for contributor

--- pages/request.vue
+++ pages/request.vue
@@ -35,7 +35,7 @@
     textarea#message(name="message" rows="5" placeholder="ハッシュタグが間違っているようです。")
 
     label(for="contributor") Contributor
-    small お名前など教えていただけるとTwitter上でお礼申し上げます。無記名でももちろん大丈夫です！
+    small 無記名でももちろん大丈夫です！
     br
     input#hashtag(type="text" name="hashtag" placeholder="@naokazu_terada")
 
