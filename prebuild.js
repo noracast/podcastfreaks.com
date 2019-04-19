e1faa1efa992c6adee4d38bd914cc7279ab09ce9
Naokazu Terada <naokazu.terada@gmail.com>
2019-04-19 11:51:15 +0900

    Use async await

--- prebuild.js
+++ prebuild.js
@@ -10,8 +10,11 @@ import wget from 'node-wget-promise'
 import wgetp from 'node-wget-promise'
 import xml2js from 'xml2js'
 import Util from './scripts/util'
+import { promisify } from 'util'
 
 let util = new Util()
+const readFile = promisify(fs.readFile)
+const xmlToJSON = promisify((new xml2js.Parser()).parseString)
 
 const RFC822 = 'ddd, DD MMM YYYY HH:mm:ss ZZ'
 const DOWNLOADS_DIR = 'static/downloads'
@@ -24,110 +27,98 @@ shell.rm('-rf', DOWNLOADS_DIR)
 shell.mkdir('-p', RSS_DIR)
 shell.mkdir('-p', COVER_DIR)
 
+let episodes_in_2weeks = []
 var latest_pubdates = []
-var episodes_in_2weeks = []
 var channels = {}
 var covers = {}
 var episodeCount = 0
 
 process.on('unhandledRejection', console.dir)
 
+const twoweeksago = moment().subtract(14, 'days').startOf('date')
+const getEpisodesIn2Weeks = (episodes, key, title)=> {
+  // Add channel info into each episodes
+  return episodes.filter((element, index, array)=> {
+    // RSS date format is RFC-822
+    return moment(element.pubDate, RFC822).isAfter(twoweeksago)
+  }).forEach(function(el) {
+    el['key'] = key
+    el['channel_title'] = title
+  })
+}
 const fetchFeed = async key => {
   const src = rss[key].feed
   const dist_rss = `${RSS_DIR}/${key}.rss`
-  await wget(src, { output: dist_rss })
-  .then( metadata => {
-    // nodeから実行する場合に、importなどが使えなかったために、async/awaitなどを使わないやり方で書いている
-    fs.readFile(`${__dirname}/${dist_rss}`, (err, xml)=> {
-      if(err) {
-        console.error(`[prebuild error] read | ${dist_rss}`)
-        throw err
-      }
-      xml2js.parseString(xml, {explicitArray: false}, (_err, json)=> {
 
-        if(_err) {
-          console.error(`[prebuild error] parse | ${dist_rss}`)
-          // throw _err // ここでエラーを発生させてしまうとビルドが継続しない
-          return
-        }
+  // Download RSS
+  await wget(src, { output: dist_rss }).catch((err) => { console.error(`[prebuild error] read | ${dist_rss}`, err) })
 
-        // Get cover image urls
-        const cover_url = util.removeQuery(_.get(json, 'rss.channel[itunes:image].$.href') || _.get(json, 'rss.channel[itunes:image].href') || _.get(json, 'rss.channel.image.url'))
-        if(cover_url){
-          covers[key] = {
-            src: cover_url,
-            dist: `${COVER_DIR}/${key}.${fileExtension(cover_url)}`
-          }
-        }
+  // Read RSS
+  const xml = await readFile(`${__dirname}/${dist_rss}`).catch(() => { return })
+  if(!xml){
+    console.error(`[prebuild error] read | ${dist_rss}`)
+    return // catch内では、fetchFeedを抜けられないのでここでreturn
+  }
+  const json = await xmlToJSON(xml).catch(() => { return })
+  if(!json){
+    console.error(`[prebuild error] parse | ${dist_rss}`)
+    return // catch内では、fetchFeedを抜けられないのでここでreturn
+  }
 
-        // count episodes
-        episodeCount += json.rss.channel.item.length
+  // Get cover image urls
+  const cover_url = util.removeQuery(_.get(json, 'rss.channel[itunes:image].$.href') || _.get(json, 'rss.channel[itunes:image].href') || _.get(json, 'rss.channel.image.url'))
+  if(cover_url){
+    covers[key] = {
+      src: cover_url,
+      dist: `${COVER_DIR}/${key}.${fileExtension(cover_url)}`
+    }
+  }
 
-        // json.rss.channel.item がなくてエラーになることがあるのでここで間引く
-        // TODO: ほんとに問題ない？
-        // if(!_.has(json, 'rss.channel.item')){
-        //   console.error('[prebuild error] json.rss.channel.item is undefined (key='+key+')')
-        //   // throw new Error('[prebuild error] json.rss.channel.item is undefined (key='+key+')')
-        // }
+  const channel = json.rss.channel[0]
+  const episodes = channel.item
+  const title = channel.title[0]
 
-        // json.rss.channel.item must be Array
-        if(!(json.rss.channel.item instanceof Array)) {
-          json.rss.channel.item = [json.rss.channel.item]
-          // TODO ページの描画時にも配列でないことで不具合が起こるので、ここで上書きしてしまいたい
-        }
+  // count episodes
+  episodeCount += episodes.length // TODOここじゃないとこで、最後にやる
 
-        // Get the latest episode's publish date
-        latest_pubdates.push({
-          id: key,
-          pubDate: json.rss.channel.item[0].pubDate
-        })
-
-        // Store episodes in last 2 weeks
-        const title = json.rss.channel.title
-        const twoweeksago = moment().subtract(14, 'days').startOf('date')
-        // RSS date format is RFC-822
-        const episodes = json.rss.channel.item.filter((element, index, array)=> {
-          return moment(element.pubDate, RFC822).isAfter(twoweeksago)
-        })
-        // Add channel info into each episodes
-        episodes.forEach(function(el) {
-          el['key'] = key
-          el['channel_title'] = title
-        })
-        episodes_in_2weeks = episodes_in_2weeks.concat(episodes)
-
-        // Save data
-        channels[key] = {
-          key,
-          title,
-          twitter: rss[key].twitter,
-          feed: rss[key].feed,
-          link: json.rss.channel.link,
-          hashtag: rss[key].hashtag,
-          cover: covers[key] ? covers[key].dist.replace(/^static/,'') : null,
-          total: json.rss.channel.item.length,
-          firstEpisodeDate: moment(_.last(json.rss.channel.item).pubDate, RFC822).format(moment.HTML5_FMT.DATETIME_LOCAL_SECONDS),
-          lastEpisodeDate: moment(_.first(json.rss.channel.item).pubDate, RFC822).format(moment.HTML5_FMT.DATETIME_LOCAL_SECONDS),
-          firstEpisodeLink: _.last(json.rss.channel.item).link,
-          lastEpisodeLink: _.first(json.rss.channel.item).link,
-          fileServer: util.getFileServer(json.rss.channel.item[0]),
-          durationAverage: util.getDurationAverage(json.rss.channel.item, dist_rss),
-          durationMedian: util.getDurationMedian(json.rss.channel.item, dist_rss),
-          desciprtion: json.rss.channel.description
-        }
-      })
-    })
-    console.log('OK!', key)
+  // Get the latest episode's publish date
+  latest_pubdates.push({
+    id: key,
+    pubDate: episodes[0].pubDate
   })
-  .catch( err => {
-    console.error('[prebuild error]', err, key)
-  })
-  console.log('done...')
+
+  episodes_in_2weeks = episodes_in_2weeks.concat(getEpisodesIn2Weeks(episodes, key, title))
+
+  // Save data
+  channels[key] = {
+    key,
+    title,
+    twitter: rss[key].twitter,
+    feed: rss[key].feed,
+    link: channel.link,
+    hashtag: rss[key].hashtag,
+    cover: covers[key] ? covers[key].dist.replace(/^static/,'') : null,
+    total: episodes.length,
+    firstEpisodeDate: moment(_.last(episodes).pubDate, RFC822).format(moment.HTML5_FMT.DATETIME_LOCAL_SECONDS),
+    lastEpisodeDate: moment(_.first(episodes).pubDate, RFC822).format(moment.HTML5_FMT.DATETIME_LOCAL_SECONDS),
+    firstEpisodeLink: _.last(episodes).link,
+    lastEpisodeLink: _.first(episodes).link,
+    fileServer: util.getFileServer(episodes[0]),
+    durationAverage: util.getDurationAverage(episodes, dist_rss),
+    durationMedian: util.getDurationMedian(episodes, dist_rss),
+    desciprtion: channel.description ? channel.description[0] : null
+  }
+
+  console.log('OK!', key)
 }
 
 (async () => {
 
-  await Promise.all(Object.keys(rss).map(async key => await fetchFeed(key)))
+  // https://qiita.com/jkr_2255/items/62b3ee3361315d55078a
+  // 並列処理
+  // await Promise.all(Object.keys(rss).map(async key => await fetchFeed(key))).catch(()=> { console.log('catch!'); return 'hoge' })
+  // 直列処理
+  for(let key of Object.keys(rss)) await fetchFeed(key)
 
   console.log('ALL DONE!!!!')
 
@@ -171,13 +162,13 @@ const fetchFeed = async key => {
         .resize(120)
         .toFile(ext_120, (err, info) => {
           if(err){
-            console.error('[prebuild error]', err, info)
+            console.error('[prebuild error]', key, err, info)
           }
         })
         .resize(60)
         .toFile(ext_60, (err, info) => {
           if(err){
-            console.error('[prebuild error]', err, info)
+            console.error('[prebuild error]', key, err, info)
           }
         })
     })
