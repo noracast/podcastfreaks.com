2e9813ef5ccda32c09ff91a8c44ed464e884c4a9
Naokazu Terada <naokazu.terada@gmail.com>
2019-04-20 18:07:25 +0900

    Add new keyword

--- scripts/fetch-twitter.js
+++ scripts/fetch-twitter.js
@@ -4,7 +4,7 @@ import TwitterFollowersCount from 'twitter-followers-count'
 
 require('dotenv').config()
 
-const tfc = TwitterFollowersCount({
+const tfc = new TwitterFollowersCount({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
