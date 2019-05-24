6ecb96a6d806c049e749e326c54cc6cf83741bde
Naokazu Terada <naokazu.terada@gmail.com>
2019-05-24 11:01:53 +0900

    Testing hello function

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,6 +1,9 @@
-exports.handler = (event, context, callback) => {
-  callback(null, {
+exports.handler = async (event, context) => {
+  console.log('submidssion created function called!')
+  console.log(event)
+  console.log(context)
+  return {
     statusCode: 200,
-    body: 'hello!'
-  })
+    body: 'aaaa'
+  };
 };
