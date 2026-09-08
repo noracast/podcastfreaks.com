d986a76f24c5b198b3397c76efd13c7eeb00aca9
Naokazu Terada <naokazu.terada@gmail.com>
2026-09-08 13:34:23 +0900

    Revert "Fix: Netlify のビルドエラーを修正（sleep パッケージを削除）"
    This reverts commit ee44e64cef6f25efac58e0f83ae5768cb2f3d22d.

--- prebuild.js
+++ prebuild.js
@@ -12,6 +12,7 @@ import PFUtil from './scripts/pf-util'
 import rss from './data/rss.json'
 import serializeError from 'serialize-error'
 import shell from 'shelljs'
+import { sleep } from 'sleep'
 import wget from 'node-wget-promise'
 import xml2js from 'xml2js'
 import { promisify } from 'util'
@@ -31,8 +32,6 @@ args.splice(0, 2) // remove not 'arg' values
 // CLI arguments list
 const NO_TWITTER = args.includes('--no-twitter') // to cancel twitter data fetching
 // ----------------
-
-const sleep = sec => new Promise(resolve => setTimeout(resolve, sec * 1000))
 
 const util = new PFUtil()
 const readFile = promisify(fs.readFile)
